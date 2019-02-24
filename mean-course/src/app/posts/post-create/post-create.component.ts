cbbe1b0f4b2eb95f020ed8fdb13282295522bf0c
Ionut Ciochinaru <ciochinaru_ionut@yahoo.com>
2019-02-24 12:53:44 +0200

    added eddit functionality

--- mean-course/src/app/posts/post-create/post-create.component.ts
+++ mean-course/src/app/posts/post-create/post-create.component.ts
@@ -1,7 +1,8 @@
-import { Component } from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {Post} from "../post.model";
 import {NgForm} from "@angular/forms";
 import {PostsService} from "../posts.service";
+import {ActivatedRoute, ParamMap} from "@angular/router";
 
 @Component({
   selector: 'app-post-create',
@@ -9,20 +10,38 @@ import {PostsService} from "../posts.service";
   styleUrls: ['./post-create.component.css']
 })
 
-export class PostCreateComponent {
+export class PostCreateComponent implements OnInit{
 
-  constructor(public postsService: PostsService){}
+  private mode = 'create';
+  private postId: string;
+  post: Post;
 
-  onAddPost(form: NgForm){
+  constructor(public postsService: PostsService, public route: ActivatedRoute){}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe((paramMap: ParamMap) => {
+      if (paramMap.has('postId')) {
+          this.mode = 'edit';
+          this.postId = paramMap.get('postId');
+          this.postsService.getPost(this.postId).subscribe(postData => {
+            this.post = {id:postData._id, title: postData.title, content: postData.content}
+          });
+      } else {
+        this.mode = 'create';
+        this.postId = null;
+      }
+    });
+  }
+
+  onSavePost(form: NgForm){
     if (form.invalid){
       return;
     }
-    const post: Post = {
-      id: null,
-      title: form.value.title,
-      content: form.value.content
-    };
-    this.postsService.addPost(form.value.title, form.value.content);
+    if (this.mode === 'create') {
+      this.postsService.addPost(form.value.title, form.value.content);
+    } else {
+      this.postsService.updatePost(this.postId, form.value.title, form.value.content);
+    }
     form.resetForm();
   }
 }
--- mean-course/src/app/posts/posts.service.ts
+++ mean-course/src/app/posts/posts.service.ts
@@ -33,6 +33,26 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
+  getPost(id: string){
+    return this.http.get<{_id:string, title: string, content: string}>('http://localhost:4100/api/posts/' + id);
+  }
+
+  updatePost(id: string, title: string, content: string) {
+    const post: Post = {
+      id: id,
+      title: title,
+      content: content
+    };
+    this.http.put('http://localhost:4100/api/posts/' + id, post)
+      .subscribe(response => {
+        const updatedPosts = [...this.posts];
+        const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+        updatedPosts[oldPostIndex] = post;
+        this.posts = updatedPosts;
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
   addPost(title: string, content: string){
     const post: Post = {id: null, title: title, content: content};
     this.http.post<{message: string, postId: string}>('http://localhost:4100/api/posts', post).subscribe((responseData) => {
